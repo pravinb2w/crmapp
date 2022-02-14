6beef3e597035ca5fe67fd0325d259d890e410de
prabhuKKannan <prabhu.k.code@gmail.com>
2022-02-14 12:09:50 +0530

    backup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid">
                         
-    <!-- start page title -->
+    <!-- start page title --> 
     <div class="row">
         <div class="col-12">
             <div class="page-title-box">
