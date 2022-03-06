b7176da56af8948540570f5e2ab0fb09b665babc
prabhuKKannan <prabhu.k.code@gmail.com>
2022-03-06 07:03:51 +0530

    update

--- resources/views/crm/account/_account_prefix.blade.php
+++ resources/views/crm/account/_account_prefix.blade.php
@@ -4,7 +4,42 @@
 </div>
 <form class="form-horizontal account_form" enctype="multipart/form-data" id="company_setting_form">
     <input type="hidden" name="type" value="{{ $type ?? '' }}">
+
+    <div class="text-end mb-3">
+        <button id="addprefRow" type="button" class="btn btn-success">Add More</button>
+        <button type="submit" class="btn btn-info">Update</button>
+    </div>
+    <table class="table table-bordered">
+        <thead class="bg-light">
+            <tr>
+                <th>Prefix</th>
+                <th>PH/DL/LD</th>
+                <th width="60px" class="text-center">Action</th>
+            </tr>
+        </thead>
+        <tbody  id="prefRow">            
+            @if( isset($prefix) && !empty($prefix))
+                @foreach ($prefix as   $item)
+                    <tr id="inputFormRow">
+                        <td style="padding: 0 !important"><input type="hidden" name="prefix_id[]" value="{{ $item->id }}"><input type="text" name="prefix_field[]" value="{{ $item->prefix_field }}" class="form-control m-input" placeholder="Prefix Field" autocomplete="off" required></td>
+                        <td style="padding: 0 !important"><input type="text" name="prefix_value[]" value="{{ $item->prefix_value }}" class="form-control m-input" placeholder="PH/DL/LD" autocomplete="off" required></td>
+                        <td class="text-center" style="padding: 0 !important"><button id="removeprefRow" type="button" class="btn btn-danger">Remove</button></td>
+                    </tr>                     
+                @endforeach           
+            @else 
+                <tr id="inputFormRow">
+                    <td style="padding: 0 !important"><input type="hidden" name="prefix_id[]" value=""><input type="text" name="prefix_field[]" value="" class="form-control m-input" placeholder="Prefix Field" autocomplete="off" required></td>
+                    <td style="padding: 0 !important"><input type="text" name="prefix_value[]" value="" class="form-control m-input" placeholder="PH/DL/LD" autocomplete="off" required></td>
+                    <td class="text-center" style="padding: 0 !important"><button id="removeprefRow" type="button" class="btn btn-danger">Remove</button></td>
+                </tr>                
+            @endif
+        </tbody>
+    </table>
+
     <div class="row mb-3">
+        <div class="col-12 mb-3 text-end">
+            
+        </div>
         <div id="prefRow">
             @if( isset($prefix) && !empty($prefix))
                 @foreach ($prefix as $item)
@@ -14,7 +49,7 @@
                             <input type="text" name="prefix_field[]" value="{{ $item->prefix_field }}" class="form-control m-input" placeholder="Prefix Field" autocomplete="off" required>
                             <input type="text" name="prefix_value[]" value="{{ $item->prefix_value }}" class="form-control m-input" placeholder="PH/DL/LD" autocomplete="off" required>
                             <div class="input-group-append">
-                                <button id="removeprefRow" type="button" class="btn btn-danger">Remove</button>
+                                <button id="removeprefRow" type="button" class="btn btn-danger"><i class="mdi mdi-trash-can"></i></button>
                             </div>
                         </div>
                     </div>
@@ -26,37 +61,34 @@
                         <input type="text" name="prefix_field[]" value="" class="form-control m-input" placeholder="Prefix Field" autocomplete="off" required>
                         <input type="text" name="prefix_value[]" value="" class="form-control m-input" placeholder="PH/DL/LD" autocomplete="off" required>
                         <div class="input-group-append">
-                            <button id="removeprefRow" type="button" class="btn btn-danger">Remove</button>
+                            <button id="removeprefRow" type="button" class="btn btn-danger"><i class="mdi mdi-trash-can"></i></button>
                         </div>
                     </div>
                 </div>
             @endif
-            </div>
-            <div class="col-12">
-                <button id="addprefRow" type="button" class="btn btn-success w-100">Add More</button>
-            </div>
-    </div>
-    
-    <div class="justify-content-end row">
-        <div class="col-12">
-            <button type="submit" class="btn btn-info w-100">Update</button>
         </div>
-    </div>
+    </div> 
 </form> 
 <script>
 
 $("#addprefRow").click(function () {
     var html = '';
-    html += '<div id="inputFormRow">';
+    html += '<div >';
     html += '<div class="input-group mb-3">';
-    html += '<input type="text" name="prefix_field[]" value="" class="form-control m-input" placeholder="Prefix Field" autocomplete="off" required>';
-    html += '<input type="text" name="prefix_value[]" value="" class="form-control m-input" placeholder="PH/DL/LD" autocomplete="off" required>';
+    html += '';
+    html += '>';
     html += '<div class="input-group-append">';
-    html += '<button id="removeprefRow" type="button" class="btn btn-danger">Remove</button>';
+    html += '';
     html += '</div>';
     html += '</div>';
 
-    $('#prefRow').append(html);
+    $('#prefRow').append(`
+        <tr id="inputFormRow">
+            <td style="padding: 0 !important"><input type="text" name="prefix_field[]" value="" class="form-control m-input" placeholder="Prefix Field" autocomplete="off" required></td>
+            <td style="padding: 0 !important"><input type="text" name="prefix_value[]" value="" class="form-control m-input" placeholder="PH/DL/LD" autocomplete="off" required</td>
+            <td class="text-center" style="padding: 0 !important"><button id="removeprefRow" type="button" class="btn btn-danger"><i class="mdi mdi-trash-can"></i></button></td>
+        </tr>
+    `);
 });
 
 // remove row
