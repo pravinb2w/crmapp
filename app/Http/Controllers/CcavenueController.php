ffb64547477378d6730bd254534fb923ed292e1c
Surendar S <duraibytes@gmail.com>
2022-08-19 07:56:45 +0530

    ccavenue payment testing

--- app/Http/Controllers/CcavenueController.php
+++ app/Http/Controllers/CcavenueController.php
@@ -19,108 +19,110 @@ class CcavenueController extends Controller
 {
     public function success_payment(Request $request)
     {
-        echo '<pre>';
-        print_r( $_POST );
+       
         $order_id = $_POST['order_id'];
         if( isset( $order_id ) && !empty( $order_id ) ) {
             $order_info = Order::where('order_id', $order_id)->first();
             $payment_info = Payment::where('order_id', $order_id)->first();
+            if( isset( $order_info ) && !empty( $order_info ) ) {
 
-            if (isset($payment_info->invoice_id) && !empty($payment_info->invoice_id)) {
-                $invoice_id = $payment_info->invoice_id;
-            } else {
-                //insert in invoice and invoice item table
-                $invoice_no = CommonHelper::get_invoice_code();
-                $ins['invoice_no'] = $invoice_no;
-                $ins['order_no'] = $order_id;
-                $ins['issue_date'] = date('Y-m-d');
-                $ins['due_date'] = date('Y-m-d');
-                $ins['customer_id'] = $order_info->customer_id;
-                $ins['address'] = $order_info->customer->address;
-                $ins['email'] = $order_info->customer->email;
+                if (isset($payment_info->invoice_id) && !empty($payment_info->invoice_id)) {
+                    $invoice_id = $payment_info->invoice_id;
+                } else {
+                    //insert in invoice and invoice item table
+                    $invoice_no = CommonHelper::get_invoice_code();
+                    $ins['invoice_no'] = $invoice_no;
+                    $ins['order_no'] = $order_id;
+                    $ins['issue_date'] = date('Y-m-d');
+                    $ins['due_date'] = date('Y-m-d');
+                    $ins['customer_id'] = $order_info->customer_id;
+                    $ins['address'] = $order_info->customer->address;
+                    $ins['email'] = $order_info->customer->email;
+        
+                    $ins['total'] = $order_info->amount;
+                    $ins['status'] = 0;
+        
+                    $invoice_id = Invoice::create($ins)->id;
+                    $product_info = Product::where('product_code', $order_info->product_code)->first();
+                    $up_data = [];
+                    $ups['invoice_id'] = $invoice_id;
+                    $ups['product_id'] = $product_info->id ?? '';
+                    $ups['description'] = '';
+                    $ups['qty'] = 1;
+                    $ups['unit_price'] = $order_info->amount;
+                    $ups['discount'] = 0;
+                    $ups['cgst'] = 0;
+                    $ups['sgst'] = 0;
+                    $ups['igst'] = 0;
+                    $ups['amount'] = $order_info->amount;
+        
+                    InvoiceItem::create($ups);
+                    $pdf_template = $request->pdf_template;
+                    $this->generatePDF($invoice_id, $pdf_template);
     
-                $ins['total'] = $order_info->amount;
-                $ins['status'] = 0;
-    
-                $invoice_id = Invoice::create($ins)->id;
-                $product_info = Product::where('product_code', $order_info->product_code)->first();
-                $up_data = [];
-                $ups['invoice_id'] = $invoice_id;
-                $ups['product_id'] = $product_info->id ?? '';
-                $ups['description'] = '';
-                $ups['qty'] = 1;
-                $ups['unit_price'] = $order_info->amount;
-                $ups['discount'] = 0;
-                $ups['cgst'] = 0;
-                $ups['sgst'] = 0;
-                $ups['igst'] = 0;
-                $ups['amount'] = $order_info->amount;
-    
-                InvoiceItem::create($ups);
-                $pdf_template = $request->pdf_template;
-                $this->generatePDF($invoice_id, $pdf_template);
-
-                $payment_info->invoice_id = $invoice_id;
-            }
-            $payment_info->name = $_POST['billing_name'];
-            $payment_info->email = $_POST['billing_email'];
-            $payment_info->contact_no = $_POST['billing_tel'];
-            $payment_info->payment_response = serialize($_POST);
-            if( strtolower($_POST['order_status']) == 'success' ) {
-                $payment_status = 'paid';
-            } else {
-                $payment_status = 'failed';
-            }
-            $payment_info->payment_status = $payment_status;
-            $payment_info->update();
-
-            $invoice = Invoice::find($invoice_id);
-            $invoice->paid_at = date('Y-m-d H:i:s');
-            $invoice->paid_amount = $_POST['amount'];
-            $invoice->approved_at = date('Y-m-d H:i:s');
-            $invoice->status = 1;
-            $invoice->update();
-
-            $order_info = Order::where('order_id', $order_id)->first();
-            $order_info->status = 'completed';
-            $order_info->update();
-
-            if( $payment_status == 'paid' ) {
-                $res_msg = ['erorr' => 'success', 'message' => 'Payment Success', 'order_no' => $order_id, 'invoice_no' => $invoice->invoice_no];
-                Session::put('razorpay_response', $res_msg);
-
-                //send email 
-                $company = CompanySettings::find(1);
-                $product_name = '';
-                if( isset($order_info->product->product_name) && !empty($order_info->product->product_name) ) {
-                    $product_name = $order_info->product->product_name;
+                    $payment_info->invoice_id = $invoice_id;
                 }
-
-                $extract = array(
-                    'company_name' => $company->site_name,
-                    'product' => $order_info->order_id . ' ' . $product_name,
-                    'amount' => $order_info->amount,
-                    'confirmed_date' => date('d M Y'),
-                );
-
-                $ins_mail = array(
-                    'type' => 'order',
-                    'type_id' => $order_info->id,
-                    'email_type' => 'success_payment',
-                    'params' => serialize($extract),
-                    'to' => $order_info->customer->email ?? 'duraibytes@gmail.com'
-                );
-                SendMail::create($ins_mail);
-                if (isset($invoice->deal_id) && !empty($invoice->deal_id)) {
-                    CommonHelper::send_payment_received_notification($invoice->deal_id);
+                $payment_info->name = $_POST['billing_name'];
+                $payment_info->email = $_POST['billing_email'];
+                $payment_info->contact_no = $_POST['billing_tel'];
+                $payment_info->payment_response = serialize($_POST);
+                if( strtolower($_POST['order_status']) == 'success' ) {
+                    $payment_status = 'paid';
+                } else {
+                    $payment_status = 'failed';
                 }
-
-                return redirect()->route('landing.index')->with('status', 'Payment completed!');
-
-            } else {
-                $res_msg = ['erorr' => 'error', 'message' => 'Payment Failed', 'order_no' => $order_id];
-                Session::put('razorpay_response', $res_msg);
-                $_SESSION['razor_response'] = $res_msg;
+                $payment_info->payment_status = $payment_status;
+                $payment_info->update();
+    
+                $invoice = Invoice::find($invoice_id);
+                $invoice->paid_at = date('Y-m-d H:i:s');
+                $invoice->paid_amount = $_POST['amount'];
+                $invoice->approved_at = date('Y-m-d H:i:s');
+                $invoice->status = 1;
+                $invoice->update();
+    
+                $order_info = Order::where('order_id', $order_id)->first();
+                $order_info->status = 'completed';
+                $order_info->update();
+    
+                if( $payment_status == 'paid' ) {
+                    
+                    $res_msg = ['erorr' => 'success', 'message' => 'Payment Success', 'order_no' => $order_id, 'invoice_no' => $invoice->invoice_no];
+                    Session::put('razorpay_response', $res_msg);
+                    
+                    //send email 
+                    $company = CompanySettings::find(1);
+                    $product_name = '';
+                    if( isset($order_info->product->product_name) && !empty($order_info->product->product_name) ) {
+                        $product_name = $order_info->product->product_name;
+                    }
+    
+                    $extract = array(
+                        'company_name' => $company->site_name,
+                        'product' => $order_info->order_id . ' ' . $product_name,
+                        'amount' => $order_info->amount,
+                        'confirmed_date' => date('d M Y'),
+                    );
+    
+                    $ins_mail = array(
+                        'type' => 'order',
+                        'type_id' => $order_info->id,
+                        'email_type' => 'success_payment',
+                        'params' => serialize($extract),
+                        'to' => $order_info->customer->email ?? 'duraibytes@gmail.com'
+                    );
+                    SendMail::create($ins_mail);
+                    if (isset($invoice->deal_id) && !empty($invoice->deal_id)) {
+                        CommonHelper::send_payment_received_notification($invoice->deal_id);
+                    }
+    
+                    return redirect()->route('landing.index')->with('status', 'Payment completed!');
+    
+                } else {
+                    $res_msg = ['erorr' => 'error', 'message' => 'Payment Failed', 'order_no' => $order_id];
+                    Session::put('razorpay_response', $res_msg);
+                    $_SESSION['razor_response'] = $res_msg;
+                }
             }
             
             return redirect()->route('landing.index')->with('status', 'Payment completed!');
